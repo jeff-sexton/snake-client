c2ba26db714ce54de46374bd2e857232031a574e
Jeff Sexton <jsexton@Jeffs-MacBook-Pro.local>
2020-03-11 13:48:23 -0700

    Added name at connection start

--- client.js
+++ client.js
@@ -2,13 +2,28 @@ const net = require('net');
 /**
  * Establishes connection with the game server
  */
-const connect = function() {
+const connect = function(name) {
+
+  let initials = '';
+  if (name.length > 3) {
+    initials = name.slice(0, 3);
+  } else {
+    initials = name;
+  }
+  
+
   const conn = net.createConnection({
     host: '192.168.88.177',
     port: 50541
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
+
+  conn.on('connect', () => {
+    console.log(`Connection Established.`);
+    conn.write(`Name: ${initials}`);
+
+  });
 
   conn.on('data', (data) => {
     console.log(data);
--- play.js
+++ play.js
@@ -4,4 +4,4 @@ const { connect } = require('./client');
 
 
 console.log('Connecting ...');
-connect();
+connect('jeff');
