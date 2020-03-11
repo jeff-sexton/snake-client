d5a8bb6629d2e34f8818b062b97b2ed6371c72e8
Jeff Sexton <jsexton@Jeffs-MacBook-Pro.local>
2020-03-11 14:15:00 -0700

    Added user input

--- play.js
+++ play.js
@@ -1,7 +1,30 @@
 const net = require('net');
 const { connect } = require('./client');
 
+/**
+ * Setup User Interface
+ * Specifically, so that we can handle user input via stdin
+ */
+const handleUserInput = (data) => {
+  //console.log(`you got here ${data}`);
+  if (data === '\u0003') {
+    process.exit();
+  }
+};
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  return stdin;
+};
+
 
 
 console.log('Connecting ...');
 connect('jeff');
+
+setupInput().on('data', handleUserInput);
+
