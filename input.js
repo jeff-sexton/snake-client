dbd15ea22610fe636d0e4e54130365767944d754
Jeff Sexton <jsexton@Jeffs-MacBook-Pro.local>
2020-03-11 16:24:49 -0700

    Added autoMove and u turn features

--- input.js
+++ input.js
@@ -1,4 +1,16 @@
 let connection;
+let moveInvervalID;
+let direction = 0;
+const directions = ['up', 'right', 'down', 'left'];
+
+const autoMove = (direction) => {
+
+  return setInterval((inst)=> {
+    connection.write(`Move: ${inst}`);
+    console.log(`Move: ${inst}`);
+  }, 75, direction);
+
+};
 
 /**
  * Setup User Interface
@@ -11,52 +23,76 @@ const handleUserInput = (data) => {
     process.exit();
   }
 
-  let direction = 0;
-  let directions = ['up', 'right', 'down', 'left'];
-
   if (data === 'w') {
     direction = 0;
-    connection.write(`Move: ${directions[direction]}`);
+    clearInterval(moveInvervalID);
+    moveInvervalID = autoMove(directions[direction]);
+    //connection.write(`Move: ${directions[direction]}`);
     console.log(directions[direction]);
-
+    
   }
   if (data === 's') {
     direction = 2;
-    connection.write(`Move: ${directions[direction]}`);
+    clearInterval(moveInvervalID);
+    moveInvervalID = autoMove(directions[direction]);
+
+    //connection.write(`Move: ${directions[direction]}`);
     console.log(directions[direction]);
     
   }
   if (data === 'a') {
     direction = 3;
-    connection.write(`Move: ${directions[direction]}`);
+    clearInterval(moveInvervalID);
+    moveInvervalID = autoMove(directions[direction]);
+    //connection.write(`Move: ${directions[direction]}`);
     console.log(directions[direction]);
     
     
   }
   if (data === 'd') {
     direction = 1;
-    connection.write(`Move: ${directions[direction]}`);
+    clearInterval(moveInvervalID);
+    moveInvervalID = autoMove(directions[direction]);
+    //connection.write(`Move: ${directions[direction]}`);
+    console.log(directions[direction]);
+
+  }
+  if (data === 'h') {
+    connection.write(`Say: u-turn!`);
+    clearInterval(moveInvervalID);
+    moveInvervalID = autoMove(directions[direction]);
+    //connection.write(`Move: ${directions[direction]}`);
     console.log(directions[direction]);
 
   }
   if (data === 'u') {
     console.log('do a u-turn!');
     connection.write(`Say: u-turn!`);
+    clearInterval(moveInvervalID);
+
     let delay = 0;
     for (let i = 0; i < 2; i++) {
       direction += 1;
+      console.log(direction);
       if (direction > directions.length - 1) {
         direction -= directions.length;
       }
 
-      setTimeout(() => {
-        connection.write(`Move: ${directions[direction]}`);
-        console.log(`Move: ${directions[direction]}`);
-      }, delay);
+      setTimeout((inst) => {
+        connection.write(`Move: ${inst}`);
+        console.log(`Move: ${inst}`);
+      }, delay, directions[direction]);
 
       delay += 50;
     }
+
+    //go back to autoMove
+    setTimeout((inst) => {
+      moveInvervalID = autoMove(inst);
+    }, delay, directions[direction]);
   }
+
+
 
 };
 
